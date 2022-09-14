3c58c8d6fff89638250c9fcd4d541167ff043f9d
Emili0 <emiliothomasking@gmail.com>
2022-09-14 15:31:33 -0700

    restructured db and functions

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,30 @@
 DROP DATABASE IF EXISTS employee_tracker;
-
 CREATE DATABASE employee_tracker;
 USE employee_tracker;
 
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
+
+
 CREATE TABLE department (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(30)
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL(10,2),
-    department_id INTEGER,
-    FOREIGN KEY (department_id) REFERENCES department(id)
-    ON DELETE SET NULL
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department_id INTEGER
 );
 
 CREATE TABLE employee (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR (30),
-    role_id INTEGER,
-    manager_id INTEGER,
-    FOREIGN KEY (role_id) REFERENCES role(id)
-    ON DELETE SET NULL
-);
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER,
+  manager_id INTEGER,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+)
